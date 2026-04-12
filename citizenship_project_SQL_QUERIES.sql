0e997207833892b32a699491afa8c20a83ebc381
agronaziri <agron-h@outlook.com>
2026-04-12 17:01:50 +0200

    Update citizenship_project_SQL_QUERIES.sql

--- citizenship_project_SQL_QUERIES.sql
+++ citizenship_project_SQL_QUERIES.sql
@@ -1,10 +1,8 @@
 SELECT *
 FROM citizenship;
 
-
 SELECT COUNT(*) AS total_rows
 FROM citizenship;
-
 
 SELECT DISTINCT "Phase"
 FROM citizenship;
@@ -18,14 +16,12 @@ SELECT
     AVG("Difficulty (1–5)") AS avg_difficulty
 FROM citizenship;
 
-
 SELECT
     "Category",
     SUM("Time (days)") AS total_time_days
 FROM citizenship
 GROUP BY "Category"
 ORDER BY total_time_days DESC;
-
 
 SELECT
     "Category",
@@ -34,14 +30,12 @@ FROM citizenship
 GROUP BY "Category"
 ORDER BY total_cost_eur DESC;
 
-
 SELECT
     "Category",
     AVG("Difficulty (1–5)") AS avg_difficulty
 FROM citizenship
 GROUP BY "Category"
 ORDER BY avg_difficulty DESC;
-
 
 SELECT
     "Phase",
@@ -50,13 +44,11 @@ FROM citizenship
 GROUP BY "Phase"
 ORDER BY total_time_days DESC;
 
-
 SELECT
     "Step Description",
     "Time (days)"
 FROM citizenship
 ORDER BY "Time (days)" DESC;
-
 
 SELECT
     "Step Description",
@@ -64,13 +56,11 @@ SELECT
 FROM citizenship
 ORDER BY "Cost (€)" DESC;
 
-
 SELECT
     "Step Description",
     "Difficulty (1–5)"
 FROM citizenship
 ORDER BY "Difficulty (1–5)" DESC, "Time (days)" DESC;
-
 
 CREATE TABLE category_summary AS
 SELECT
@@ -81,11 +71,9 @@ SELECT
 FROM citizenship
 GROUP BY "Category";
 
-
 SELECT *
 FROM category_summary
 ORDER BY total_time_days DESC;
-
 
 CREATE TABLE phase_summary AS
 SELECT
@@ -95,7 +83,6 @@ SELECT
     AVG("Difficulty (1–5)") AS avg_difficulty
 FROM citizenship
 GROUP BY "Phase";
-
 
 SELECT *
 FROM phase_summary
